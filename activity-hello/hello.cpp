140375d0404ebf3390e25cd2a361f48d42fd470d
WesleyWotring <wwotring@uncc.edu>
2022-01-13 14:23:51 -0500

    hostname solution

--- activity-hello/hello.cpp
+++ activity-hello/hello.cpp
@@ -1,4 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 int main () {
 
-  return 0;
+  char host[HOST_NAME_MAX + 1];
+  //char array called host that hast the maximum of the name + 1 additional for size
+
+  gethostname(host, HOST_NAME_MAX +1);
+    //call the gethostname mathod to get the name that will iterate through the second variable being the length
+    
+    printf("Host: %s\n", host);
+    //print the host name
+    
+    return EXIT_SUCCESS;
 }
