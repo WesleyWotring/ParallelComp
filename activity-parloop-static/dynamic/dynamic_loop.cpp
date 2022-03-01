5d875dd3b69f583f3e7d6e4231c81ffdf62b6f5f
WesleyWotring <wwotring@uncc.edu>
2022-03-01 00:39:57 -0500

    dynamic solution

--- activity-parloop-static/dynamic/dynamic_loop.cpp
+++ activity-parloop-static/dynamic/dynamic_loop.cpp
@@ -2,15 +2,28 @@
 #define __DYNAMIC_LOOP_H
 
 #include <functional>
+#include <thread>
+#include <vector>
+#include <iostream>
+#include <mutex>
+
 
 class DynamicLoop {
-private:
-  // @brief you will want to have class members to store the number of threads
-  // and the desired granularity. Add a public setter functions for these
-  // class members.
 
+private:
+  // @brief you will want to have class member to store the number of threads
+  // add a public setter function for this member.
+  int nbthreads;
+  int granularity;
 public:
   // @breif write setters here.
+  void setThreads(int nbt){
+    this -> nbthreads = nbt;
+  }
+
+   void setGranularity(int gran){
+    this -> granularity = gran;
+  }
 
 
 
@@ -21,7 +34,7 @@ public:
   /// beg, no greater than end, in inc increment. These execution may
   /// be in parallel
   void parfor (size_t beg, size_t end, size_t inc,
-	       std::function<void(int)> f) {
+           std::function<void(int)> f) {
     for (size_t i=beg; i<end; i+= inc) {
       f(i);
     }
@@ -45,19 +58,80 @@ public:
   /// Once the iterations are complete, each thread will execute after
   /// on the TLS object. No two thread can execute after at the same time.
   template<typename TLS>
-  void parfor (size_t beg, size_t end, size_t increment,
-	       std::function<void(TLS&)> before,
-	       std::function<void(int, TLS&)> f,
-	       std::function<void(TLS&)> after
-	       ) {
-    TLS tls;
-    before(tls);    
-    for (size_t i=beg; i<end; i+= increment) {
-      f(i, tls);
+  void parfor(size_t beg, size_t end, size_t increment,
+           std::function<void(TLS&)> before,
+           std::function<void(int, TLS&)> f,
+           std::function<void(TLS&)> after
+           ) {
+    std::vector<std::thread> mythread;
+    //tls holder
+    size_t chunk = end / granularity;
+    std::vector<TLS> tlsVect;
+    TLS tls[chunk];
+    std::mutex mu;
+    
+    //chunk allocation
+    std::vector<bool> checker(chunk, false);
+    size_t sharedVariable = 0;
+    
+   
+    //int numerator = 1;
+    for(size_t b = 0; b < chunk; b++){
+       TLS tlsBefore;
+       tlsVect.push_back(tlsBefore);
+       before(tlsVect[b]);
     }
-    after(tls);
+    
+    for(size_t j = 0; j < nbthreads; j++){
+       //before(tls[j]);
+
+           
+    mythread.push_back(std::thread([&]() {
+        while(sharedVariable < chunk){
+          bool threadCheck = false;
+          mu.lock();
+          checker[sharedVariable] = true;
+
+          size_t min = sharedVariable * granularity;
+          size_t max = (min + granularity);
+          TLS& tempTLS = tlsVect[sharedVariable];
+          //std::cout<<tlsVect[sharedVariable]<< " \n";
+          
+          sharedVariable++;
+          mu.unlock();
+        
+        
+         for (size_t i=min; i< max; i+= increment) {
+             //std::cout<<tempTLS<< " \n";
+             f(i, tempTLS);
+         }
+         }
+                   }));
+
+      
+     
+      
+          //std::cout<<tlsVect[1]<< " ";
+    }
+
+     //leave this alone
+     std::for_each(mythread.begin(), mythread.end(), [](std::thread &t){
+        t.join();
+    });
+
+     for(auto& t: tlsVect){
+     //std::cout<< t << " \n";
+      after(t);
+
+    }
+     
+    //join threads
+   
+
+    
+   
   }
-  
+ 
 };
 
 #endif
--- activity-parloop-static/dynamic/dynamic_main.cpp
+++ activity-parloop-static/dynamic/dynamic_main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <string.h>
+#include <cmath>
+#include <functional>
 #include <chrono>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dynamic_loop.cpp"
 
 #ifdef __cplusplus
@@ -18,12 +20,88 @@ float f4(float x, int intensity);
 }
 #endif
 
+//add granularity
+double calc_numerical_integration(int functionid, int a, int b, int n, int intensity, int nb_threads, int granularity){
+    DynamicLoop dynLoop;
+    dynLoop.setThreads(nb_threads);
+    dynLoop.setGranularity(granularity);
+    
+    double x = (double) a;
+    double y = (double) b;
+    
+    double res = 0;
+    double resG = 0;
+    double temp;
+    //switch case should be in for loop
+    dynLoop.parfor<double>(0, n, 1,
+
+			 //before
+			  [&](double& tls) -> void{
+						tls = 0;
+			  },
+
+			 //mid
+
+	[&](int i, double& tls) -> void{
+
+        temp = (x+(i+0.5) * ((y-x)/n));
+       
+        switch(functionid){
+    
+        case 1 : 
+            tls  += f1(temp, intensity);
+            break;
+        case 2 : 
+            tls += f2(temp, intensity);
+            break;
+        case 3 : 
+           tls  += f3(temp, intensity);
+            break;
+        case 4 : 
+            tls += f4(temp, intensity);
+        }
+	//tls=4444.884;
+	},
+	         //after
+	        [&](double& tls) -> void{
+		    res += tls;
+		}
+    );
+    
+    res = ((y-x)/n) * res;
+    
+    return res;
+}
+
+
 int main (int argc, char* argv[]) {
+
+  int function_id = atoi(argv[1]);
+  int a = atoi(argv[2]);
+  int b = atoi(argv[3]);
+  int n = atoi(argv[4]);
+  int intensity = atoi(argv[5]);
+  int nbthreads = atoi(argv[6]);
+  int granularity = atoi(argv[7]);
 
   if (argc < 8) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <granularity>"<<std::endl;
     return -1;
   }
 
+  auto start = std::chrono::system_clock::now(); //
+  double r = calc_numerical_integration(function_id, a, b, n, intensity, nbthreads, granularity); //
+  auto end = std::chrono::system_clock::now(); //
+
+  std::chrono::duration<double> elapsed_seconds = end-start; //
+  std::time_t end_time = std::chrono::system_clock::to_time_t(end); //
+
+  printf("%lf\n", r);
+  
+  fprintf(stderr, "%f\n", elapsed_seconds.count()); //
+  
+
   return 0;
 }
+
+
